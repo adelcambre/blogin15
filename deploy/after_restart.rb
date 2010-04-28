7973fab5ce2998e99e17bd8f82112756ea7eca2a
Andy Delcambre <adelcambre@engineyard.com>
2010-04-28 11:22:00 -0700

    Tweak after_restart

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,2 +1,2 @@
 run "echo 'release_path: #{release_path}' >> #{shared_path}/after_restart.rb_logs.log"
-run "echo '#{node.inspect}' > #{shared_path}/node.log"
+run "echo '#{node["ruby_version"]}' > #{shared_path}/node.log"
