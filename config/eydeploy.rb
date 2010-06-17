07202bf8dc0d57e2b2593dcc8b56c9a61f67fa05
Andy Delcambre <adelcambre@engineyard.com>
2010-06-17 15:50:27 -0700

    c not x

--- config/eydeploy.rb
+++ config/eydeploy.rb
@@ -28,7 +28,7 @@ end
 def copy_repository_cache
   # Don't need to do this, just make sure current is linked to
   # cached-copy
-  run "ln -nfs #{x.repository_cache} #{c.current_path}"
+  run "ln -nfs #{c.repository_cache} #{c.current_path}"
 end
 
 def symlink(*args)
