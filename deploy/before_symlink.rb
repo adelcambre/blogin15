a21cec60d51a2dae6ddc80b5cb4007662b712493
Andy Delcambre <adelcambre@engineyard.com>
2010-07-08 11:16:09 -0700

    change the before symlink hook a bit

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,3 @@
 on_app_master do
-  File.open(File.join(shared_path, 'before_symlink.log'), 'w') {|f| f.puts 'before_symlink' }
+  File.open(File.join(shared_path, 'before_symlink.log'), 'w') {|f| f.puts 'before_symlink new one' }
 end
