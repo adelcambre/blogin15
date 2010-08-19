5e54b23fff13188061377f99048c6692c195873b
Andy Delcambre <adelcambre@engineyard.com>
2010-08-18 17:00:04 -0700

    secret_token

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '41fa2be86699af984f1204f20c7652162447eea8c07fc29510f7c40bb1c09148e4ef3c35a7316e16dfa3d96fa8591c125ae455b93b0c44f383d8e1bc93e93a7b'
+BlogIn15::Application.config.secret_token = '41fa2be86699af984f1204f20c7652162447eea8c07fc29510f7c40bb1c09148e4ef3c35a7316e16dfa3d96fa8591c125ae455b93b0c44f383d8e1bc93e93a7b'
