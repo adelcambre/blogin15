052db75d788b89f7d5e8ff7265bc10730ae541a7
Andy Delcambre <adelcambre@engineyard.com>
2010-08-18 17:02:33 -0700

    secret_token rd 2

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Blogin15
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.secret_token = '41fa2be86699af984f1204f20c7652162447eea8c07fc29510f7c40bb1c09148e4ef3c35a7316e16dfa3d96fa8591c125ae455b93b0c44f383d8e1bc93e93a7b'
 
     # Add additional load paths for your own custom dirs
     # config.load_paths += %W( #{config.root}/extras )
